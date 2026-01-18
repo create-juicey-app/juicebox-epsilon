01499bc7b0f07d960856438ead513ea8352481f6
root <root@debian-s-1vcpu-2gb-amd-fra1-01>
2026-01-18 17:20:34 +0100

    astro sentry spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), sentry(), spotlightjs()],
+  server: {
+    allowedHosts: ['epsilon.juicey.dev'],
+  }
+
 });
